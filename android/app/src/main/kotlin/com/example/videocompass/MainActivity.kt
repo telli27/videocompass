160e71c4eb07d950d8fc8feea549836fb20a9267
telli27 <huseyintelli30@gmail.com>
2022-10-14 09:31:04 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/videocompass/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.videocompass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_selector_macos
+import path_provider_macos
+import video_compress
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
+}
